04e5ed6d0a8778e472142397363c85eba49840f1
lodhikomal <komallodhi720@gmail.com>
2023-10-12 10:44:01 +0530

    2nd

--- src/component/Weather2.js
+++ src/component/Weather2.js
@@ -7,6 +7,38 @@ function Weather2() {
         <div className="heading">
           <h3>Today's Highlights</h3>
         </div>
+        <div className="container">
+          <div>
+            <span>uv Index</span>
+            <p>0</p>
+            <span>low</span>
+          </div>
+          <div>
+            <span>wind status</span>
+            <p>0</p>
+            <span>km/h</span>
+          </div>
+          <div>
+            <span>Sunrise & Sunset</span>
+            <p>0</p>
+            <span>0</span>
+          </div>
+          <div>
+            <span>Humidity</span>
+            <p>0</p>
+            <span>Normal</span>
+          </div>
+          <div>
+            <span>visibility</span>
+            <p>0</p>
+            <span>Normal</span>
+          </div>
+          <div>
+            <span>Air Quality</span>
+            <p>0</p>
+            <span>Normal</span>
+          </div>
+        </div>
       </div>
     </div>
   );
