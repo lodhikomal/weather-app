dc96407e620d45a92da2d1ecaccf09b1686d675f
lodhikomal <komallodhi720@gmail.com>
2023-10-13 13:43:00 +0530

    add time

--- src/component/Weather2.js
+++ src/component/Weather2.js
@@ -1,14 +1,27 @@
 import React from "react";
 import "./Weather2Style.css";
+import moment from "moment";
 
 function Weather2(props) {
   // console.log(props.value?.weather?.wind, "check");
+  const timeStamp = props.value?.weather?.sys?.sunrise;
+  const timeStamp1 = props.value?.weather?.sys?.sunset;
+  const date = timeStamp * 1000;
+  const date1 = timeStamp1 * 1000;
+  console.log(new Date(date1), "test date");
+  // if (weatherData.status == "loading") {
+  // } else {
+  //   return;
+  // }
+
+  if (props.value.status == "loading") return <h1>Loading...</h1>;
   return (
     <div>
       <div className="wrapper">
         <div className="heading">
           <h3>Today's Highlights</h3>
         </div>
+
         <div className="container">
           <div>
             <span>uv Index</span>
@@ -22,8 +35,11 @@ function Weather2(props) {
           </div>
           <div>
             <span>Sunrise & Sunset</span>
-            <p>0</p>
-            <span>{props.value?.weather?.sys?.sunrise}</span>
+            <p>{moment(date).calendar()}</p>
+
+            <p>{moment(date1).calendar()}</p>
+            <br></br>
+            <span>{date}</span>
             <span>{props.value?.weather?.sys?.sunset}</span>
           </div>
           <div>
